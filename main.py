0c642739a028e4938932a0c23f1f79fce8c1eb31
RockChinQ <1010553892@qq.com>
2023-07-28 19:03:21 +0800

    feat: 简单功能

--- main.py
+++ main.py
@@ -1,49 +1,37 @@
 from pkg.plugin.models import *
 from pkg.plugin.host import EventContext, PluginHost
 
-"""
-在收到私聊或群聊消息"hello"时，回复"hello, <发送者id>!"或"hello, everyone!"
-"""
-
-
 # 注册插件
-@register(name="Hello", description="hello world", version="0.1", author="RockChinQ")
-class HelloPlugin(Plugin):
+@register(name="Webwlkr", description="基于GPT的函数调用能力，为QChatGPT提供联网功能", version="0.1", author="RockChinQ")
+class WebwlkrPlugin(Plugin):
 
     # 插件加载时触发
     # plugin_host (pkg.plugin.host.PluginHost) 提供了与主程序交互的一些方法，详细请查看其源码
     def __init__(self, plugin_host: PluginHost):
         pass
 
-    # 当收到个人消息时触发
-    @on(PersonNormalMessageReceived)
-    def person_normal_message_received(self, event: EventContext, **kwargs):
-        msg = kwargs['text_message']
-        if msg == "hello":  # 如果消息为hello
+    @on(ContentFunction)
+    def visit_page(lnk: str):
+        """Visit the provided link and return the plain text content.
+        
+        Args:
+            lnk(str): The link to visit.
 
-            # 输出调试信息
-            logging.debug("hello, {}".format(kwargs['sender_id']))
+        Returns:
+            str: The plain text content of the page.
+        """
+        import requests
+        from bs4 import BeautifulSoup
 
-            # 回复消息 "hello, <发送者id>!"
-            event.add_return("reply", ["hello, {}!".format(kwargs['sender_id'])])
+        r = requests.get(lnk)
+        soup = BeautifulSoup(r.text, 'html.parser')
 
-            # 阻止该事件默认行为（向接口获取回复）
-            event.prevent_default()
+        s = soup.get_text()
 
-    # 当收到群消息时触发
-    @on(GroupNormalMessageReceived)
-    def group_normal_message_received(self, event: EventContext, **kwargs):
-        msg = kwargs['text_message']
-        if msg == "hello":  # 如果消息为hello
+        if len(s) >= 256:
+            return s[:256]
 
-            # 输出调试信息
-            logging.debug("hello, {}".format(kwargs['sender_id']))
-
-            # 回复消息 "hello, everyone!"
-            event.add_return("reply", ["hello, everyone!"])
-
-            # 阻止该事件默认行为（向接口获取回复）
-            event.prevent_default()
+        return s
 
     # 插件卸载时触发
     def __del__(self):
