b21280a51b9470c853325e402f24d07e639c1fba
RockChinQ <1010553892@qq.com>
2023-07-30 19:20:13 +0800

    feat: 自动带ua

--- sites/model.py
+++ sites/model.py
@@ -1,5 +1,6 @@
 import re
 import requests
+import random
 from bs4 import BeautifulSoup
 
 
@@ -28,6 +29,20 @@ def site(regexp: list[str]):
     return wrapper
 
 
+user_agents = [
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:89.0) Gecko/20100101 Firefox/89.0',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:88.0) Gecko/20100101 Firefox/88.0',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Version/14.1.2 Safari/537.36',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Version/14.1 Safari/537.36',
+    'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:89.0) Gecko/20100101 Firefox/89.0',
+    'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:88.0) Gecko/20100101 Firefox/88.0'
+]
+
+
 class SiteAdapterBase:
     """网站内容适配器的基类"""
 
@@ -44,7 +59,10 @@ class SiteAdapterBase:
         """
         r = requests.get(
             url,
-            timeout=timeout
+            timeout=timeout,
+            headers={
+                'User-Agent': random.choice(user_agents)
+            }
         )
         
         return r.status_code, r.text
